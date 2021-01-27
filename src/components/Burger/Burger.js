3ecc3bb59966608b95653ca1276ffd10b3936659
Matthew Oballe <oballematt@gmail.com>
2021-01-27 12:46:15 -0600

    used js object.keys method to transform the state object in burgerbuilder.js to an array. Using the new array created by the key method I used the map method and used the sread operator to return a new array with the values defined in the state of burgerbuilder. Then I used reduce to condense it into one array rather than a separate array for each state value.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
+import styles from './Burger.module.css';
+import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
+
 const burger = (props) => {
+    let newIngredients = Object.keys(props.ingredients)
+    .map(igKey =>  {
+        return [...Array(props.ingredients[igKey])]
+        .map((_, i) => {
+            return <BurgerIngredients key={igKey + i} type={igKey} />
+        });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    if (newIngredients.length === 0) {
+        newIngredients = <p>Please start adding ingredients</p>
+    }
     return (
-        <div>
-            
+        <div className={styles.burger}>
+            <BurgerIngredients type="bread-top" />
+            {newIngredients}
+            <BurgerIngredients type="bread-bottom" />
         </div>
     )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 import Aux from "../../hoc/Auxiliary"
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: 
+            {
+            lettuce: 2,
+            bacon: 1,
+            cheese: 1,
+            meat: 1
+        }
+    }
+
     render () {
         return (
             <Aux>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
