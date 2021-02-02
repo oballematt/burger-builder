d4778fa52202a3d961fe0cfaa0969b6c02592ec1
Matthew Oballe <oballematt@gmail.com>
2021-02-02 10:42:49 -0600

    setting up firebase backend, sending dummy data to test.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
     lettuce: 0.5,
@@ -79,7 +80,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('Not Just Yet!')
+       // alert('Not Just Yet!')
+       const order = {
+           ingredients: this.state.ingredients,
+           price: this.state.totalPrice,
+           customer: {
+               name: "Matt Oballe",
+               address: {
+                   street: 'Teststreet 1',
+                   zipCode: '78751',
+                   country: 'United States'
+               },
+               email: 'test@test.com'
+           },
+           deliveryMethod: 'fastest'
+       }
+        axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
     }
 
     render () {
